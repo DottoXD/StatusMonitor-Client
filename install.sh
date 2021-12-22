c34d1a0e985a1f8a239093dceec1a3f5720e9f56
DottoXD <80960175+DottoXD@users.noreply.github.com>
2021-12-22 21:29:50 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+
+if [ $USER != root ]
+ then
+  printf "Please run this as root! \n"
+  exit
+fi
+apt install curl
+curl -sL https://deb.nodesource.com/setup_17.x | sudo bash -
+apt install -y nodejs git
+
+content=wget https://pastebin.com/raw/43DphpG9
+
+
+mv ./43DphpG9 /lib/systemd/system/statusmonitor.service
+
+mkdir /etc/statusmonitor && cd /etc/statusmonitor
+git clone https://github.com/DottoXD/statusmonitor-client .
+
+npm install
+
+systemctl enable --now statusmonitor
+printf "StatusMonitor - Client by Dotto has been installed! Make sure to configure /etc/statusmonitor/config.json and then systemctl restart statusmonitor!"
