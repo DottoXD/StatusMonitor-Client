b42ca4efe2edb1d108c2675465a66d28abe2b496
DottoXD <80960175+DottoXD@users.noreply.github.com>
2021-12-22 21:47:08 +0100

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+if [ $USER != root ]
+ then
+  printf "Please run this as root! \n"
+  exit
+fi
+
+systemctl disable --now statusmonitor
+apt remove -y nodejs git
+rm -r ./etc/statusmonitor
+
+printf "StatusMonitor - Client by Dotto has been uninstalled!"
